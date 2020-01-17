b4f6faeaf52f659770da15beabe15caf401d1646
Kathryn Wohl <ktwohl@gmail.com>
2020-01-17 14:46:16 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let bandNumber = 1
+
+const takeNumber = function () {
+    /*
+        Write your awesome code here. See comments
+        below for what should be returned.
+    */
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
