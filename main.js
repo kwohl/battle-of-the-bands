8a065aa92d43192f52602f8e1c810b5fbf848719
Kathryn Wohl <ktwohl@gmail.com>
2020-01-17 15:07:43 -0600

    take a number function added and tested

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-let bandNumber = 1
+let bandNumber = 0
 
-const takeNumber = function () {
-    /*
-        Write your awesome code here. See comments
-        below for what should be returned.
-    */
-}
+const takeNumber = function (bandName) {
+    bandNumber++
+    return `${bandNumber}. ${bandName}`;
+   }
 
 const scum = takeNumber("Galactic Scum")
 console.log(scum)  // This should print "1. Galactic Scum" in the console
 
 const under = takeNumber("Underdogs")
 console.log(under)  // This should print "2. Underdogs" in the console
+
+const beatles = takeNumber("The Beatles")
+console.log(beatles);
